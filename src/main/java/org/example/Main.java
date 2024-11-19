1fd57adb2b1784bf9de732b0004d58ecb93f15ef
Coralee Long <coralee@coralees-mbp.localdomain>
2024-11-19 16:40:04 +0100

    create merge conflicts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,15 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        System.out.println("test for merge conflicts");        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("hallo von Group");
         System.out.println("example with master branch (which is not main branch)");
         System.out.println("Hallo");
 
 
+        System.out.println("test for merge conflicts");
+        System.out.println("test for merge conflicts");
         System.out.println("test for merge conflicts");
     }
 }
